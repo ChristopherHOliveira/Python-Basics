a200bc6c88b917a28ec40c867cee684b82e4265b
Christopher H. Oliveira <christopher.h.oliveira@gmail.com>
2021-03-12 07:37:20 -0300

    Update Função_média_lista.py

--- Função_média_lista/Função_média_lista.py
+++ Função_média_lista/Função_média_lista.py
@@ -1,16 +1,21 @@
+# criando a função 'media', que retorna a soma de 'm' dividido pelo tamanho de 'm':
 def media(m):
     return(sum(m)/len(m))
 
+# criando uma lista vazia:
 lst = []
 
+# pedindo para o usiário digitar um valor de tamanho da lista:
 tam_lst = float(input("Digite o tamanho da lista: "))
 
+# teste para aceitar apenas números inteiros:
 while int(tam_lst) != tam_lst:
     tam_lst = float(input("Ops, apenas números inteiros! Digite novamente: "))
-
 print("Ok")    
 
+# pedindo para o usuário digitar os itens da lista:
 for x in range(int(tam_lst)):
     lst.append(float(input(f"Digite os {tam_lst} itens da lista, um por um: ")))
 
+# saída da média dos números da lista:
 print(f"A média dos valores contidos na lista é {media(lst)}")
